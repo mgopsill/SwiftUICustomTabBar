ff95a87e3bb833a75d33b259c64b25d7fc05babc
Mike Gopsill <m.gopsill@gmail.com>
2020-03-12 07:44:45 +0000

    Initial Commit

---
+++ CustomTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTabBar
+//
+//  Created by Mike Gopsill on 12/03/2020.
+//  Copyright © 2020 Ford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
